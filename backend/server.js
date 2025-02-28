2f2f23ab77b25d24e71b27560595d9a89a00c05e
Kartik <kartikchhoker108@gmail.com>
2025-02-28 16:06:50 +0530

    Fixed mongoose import error

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const Todo = require("./models/ToDo");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = 5000;
